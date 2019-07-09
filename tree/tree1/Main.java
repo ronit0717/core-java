c8a5f4aa9c3195e9b53f1d5acb47d3611a87f70e
RONIT CHATTOPADHYAY <ronit@yourdost.com>
2019-07-09 18:04:21 +0530

    Tree code added

--- tree/tree1/Main.java
+++ tree/tree1/Main.java
@@ -5,6 +5,25 @@ public class Main{
         System.out.println("Executing Tree Main");
 
         Tree tree = new Tree();
-        
+
+        System.out.println("Print1:");
+        tree.printTreeLevelOrder();
+
+        tree.insertNode(1);
+        tree.insertNode(2);
+        tree.insertNode(3);
+
+        System.out.println("Print2:");
+        tree.printTreeLevelOrder();
+
+        tree.insertNode(4);
+        tree.insertNode(5);
+
+        System.out.println("Print3:");
+        tree.printTreeLevelOrder();
+
+        tree.insertNode(6);
+        System.out.println("Print4:");
+        tree.printTreeLevelOrder();
     }
 }
--- tree/tree1/Tree.java
+++ tree/tree1/Tree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+
 public class Tree{
     Node head;
 
@@ -7,6 +10,71 @@ public class Tree{
 
     public Tree(int val){
         Node node = new Node(val);
-        Tree(node);
+        new Tree(node);
+    }
+
+    public Tree(){
+        this.head = null;
+    }
+
+    public void insertNode(int val){
+        //Inorder insertion of node
+
+        Node node = new Node(val);
+
+        if(head == null){
+            head = node;
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(this.head);
+
+        while(!q.isEmpty()){
+            Node temp = q.peek();
+            q.remove();
+
+            if(temp.left == null){
+                temp.left = node;
+                break;
+            }else{
+                q.add(temp.left);
+            }
+
+            if(temp.right == null){
+                temp.right = node;
+                break;
+            }else{
+                q.add(temp.right);
+            }
+        }
+    }
+
+    public void printTreeLevelOrder(){
+        if(this.head == null){
+            System.out.println("Tree is empty");
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(this.head);
+        StringBuilder sb = new StringBuilder();
+
+        while(!q.isEmpty()){
+            Node temp = q.peek();
+            sb.append(temp.value+" ");
+            
+            q.remove();
+
+            if(temp.left != null){
+                q.add(temp.left);
+            }
+
+            if(temp.right != null){
+                q.add(temp.right);
+            }
+        }
+
+        System.out.println(sb);
     }
 }
