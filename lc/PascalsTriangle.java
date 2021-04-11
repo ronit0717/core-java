cffb0352eb5e55d482e0f38e6aeab6e17c57c052
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-04-11 10:24:44 +0530

    Pascal's triangle Consize solution nut not faster

--- lc/PascalsTriangle.java
+++ lc/PascalsTriangle.java
@@ -1,16 +1,13 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 1; i <= numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (j = 1; j <= i; j++) {
-                if (j == 1 || j == i) {
-                    row.add(1);
-                } else {
-                    row.add(result.get(i - 2).get(j - 2) + result.get(i - 2).get(j - 1));
-                }
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
             }
-            result.add(row);
+            result.add(new LinkedList<>(row));
         }
         return result;
     }
