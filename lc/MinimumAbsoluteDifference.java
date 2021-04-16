57d46a6a5233b1e139e36e449253bb6f7dd58352
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-04-16 21:12:02 +0530

    Maximum absolute difference explanation

--- lc/MinimumAbsoluteDifference.java
+++ lc/MinimumAbsoluteDifference.java
@@ -19,3 +19,41 @@ class Solution {
         return res;
     }
 }
+
+/* To determine max of function | A[i] - A[j] | - | i - j |
+
+        A= [1,  3,  -1]
+        i= [1,  2,   3]
+A[i]+i   = [2,  5,   4]  diffMax = 3 (i = 2, j = 1)
+A[i]-i   = [0,  1,  -4]  diffMax = 5 (i = 1, j = 2) ans=5
+
+
+
+Explanation:
+In above example, there can be total 9 combinations of i and j
+
+    i   <   j       i   >   j       i   =   j
+    ==========     ===========      =========
+    1       2       2       1       1       1
+    1       3       3       1       2       2
+    2       3       3       2       3       3
+
+For i = j, the function always returns 0, ignoring this case
+
+There are 4 total ways the function gets defined based on the values of A[i], A[j], i  and j. But there are distinct two cases
+(A[i] - i) - (A[j] - j)  => A[i] > A[j] and i > j  (Equation 1)
+this is same as A[i] < A[j] and i < j, because i and j values get interchanged
+
+(A[i] + i) - (A[j] + j)  => A[i] > A[j] and i < j  (Equation 2)
+this is same as A[i] < A[j] and i > j, because i and j values get interchanged
+
+For max value in each equation, the first part of the equation needs to be maximum and second part needs to be min
+
+For equation 1
+arr of A[i]+i   => [2,  5,   4]  diffMax = 3 (when i = 2, j = 1)
+
+For equation 2
+arr of A[i]-i   => [0,  1,  -4]  diffMax = 5 (when i = 1, j = 2)
+
+overall for our function, thus maximum abs difference = 5 (when i = 1, j = 2)
+*/
