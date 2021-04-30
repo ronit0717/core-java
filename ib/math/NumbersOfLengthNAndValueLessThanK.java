5b09c84fd8416bc80e1072f04171ae744daa2cae
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-05-01 01:23:22 +0530

    Numbers of length N and value less than K - optimized

--- ib/math/NumbersOfLengthNAndValueLessThanK.java
+++ ib/math/NumbersOfLengthNAndValueLessThanK.java
@@ -3,59 +3,68 @@ public class Solution {
         if (B == 0) {
             return 0;
         }
-        int cStrLen = Integer.toString(C).length();
+        
+        if (A == null || A.size() == 0) {
+            return 0;
+        }
+        
+        Set<Integer> aSet = new HashSet<>();
+        for (int d : A) {
+            aSet.add(d);
+        }
+        A.clear();
+        A.addAll(aSet);
+        
+        
+        String cStr = Integer.toString(C);
+        int cStrLen = cStr.length();
         if (B < cStrLen) {
-            if (B == 1) {
-                return A.size();
-            }
-            int prod = 1;
-            for (int i = 0; i < B; i++) {
-                if (i == 0 && A.get(i) == 0) {
-                    prod *= (A.size() - 1);
-                    continue;
-                }
-                prod *= A.size();
-            }
-            return prod;
+            return (A.get(0) == 0 && B > 1) ? ((A.size() - 1) * (int)Math.pow(A.size(), (B - 1))) : (int)Math.pow(A.size(), B);
         } else if (B > cStrLen) {
             return 0;
         }
-        int sum = 0;
-        for (int i = 0; i < A.size(); i++) {
-            if (B != 1 && A.get(i) == 0) {
-                continue;
-            }
-            StringBuilder sb = new StringBuilder().append(A.get(i));
-            sum += checkSum(sb, A, B, C, 1);
-        }
-        return sum;
-    }
 
-    private static int checkSum(StringBuilder sb,
-        ArrayList < Integer > A,
-        int B,
-        int C,
-        int count) {
         int sum = 0;
-        int check = Integer.valueOf(sb.toString());
-        if (count >= B) {
-            if (check < C) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        if (check > C) {
-            return 0;
-        } else {
-            for (int i = 0; i < A.size(); i++) {
-                StringBuilder sbNew = new StringBuilder(sb.toString()).append(A.get(i));
-                int newSum = checkSum(sbNew, A, B, C, count + 1);
-                if (newSum > 0) {
-                    sum += newSum;
-                } else {
-                    break;
+        boolean overlap = false;
+        for (int i = 0; i < B; i++) {
+            if (i == 0) {
+                int count = 0;
+                for (int j = 0; j < A.size(); j++) {
+                    if (A.get(j) == 0 && B > 1) {
+                        continue;
+                    }
+                    int val = cStr.charAt(i) - '0';
+                    if (A.get(j) <= val) {
+                        count++;
+                    }
+                    if (A.get(j) == val) {
+                        overlap = true;
+                    }
                 }
+                sum = count * (int)Math.pow(A.size(), (B - 1));
+            } else {
+                overlap = false;
+                int count = 0;
+                int val = cStr.charAt(i) - '0';
+                if (i == (B - 1)) {
+                    for (int j = 0; j < A.size(); j++) {
+                        if (A.get(j) >= val) {
+                            count++;
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < A.size(); j++) {
+                        if (A.get(j) > val) {
+                            count++;
+                        } else if (A.get(j) == val) {
+                            overlap = true;
+                        }
+                    }
+                }
+                sum -= count * Math.pow(A.size(), (B - i - 1));
+            }    
+            if (!overlap) {
+                break;
             }
         }
         return sum;
