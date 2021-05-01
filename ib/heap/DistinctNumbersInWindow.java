1d7d8d1b8ea5d6ffee3203489436321f9ad4f5a3
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-05-02 03:36:55 +0530

    Distinct numbers in window - memory optimized

--- ib/heap/DistinctNumbersInWindow.java
+++ ib/heap/DistinctNumbersInWindow.java
@@ -4,51 +4,30 @@ public class Solution {
             return null;
         }
         ArrayList < Integer > result = new ArrayList < > ();
-        if (B == 1) {
-            for (int i : A) {
-                result.add(1);
-            }
-            return result;
-        }
-        ArrayList < HashMap < Integer, Integer >> mapList = new ArrayList < > ();
+        HashMap<Integer, Integer> memMap = new HashMap<>();
         HashMap < Integer, Integer > currMap = null;
         for (int i = 0; i < A.size(); i++) {
-            if (i == 0) {
-                currMap = new HashMap<>();    
-            }
-            int count = 1;
-            if (currMap.containsKey(A.get(i))) {
-                count += currMap.get(A.get(i));
-            }
-            currMap.put(A.get(i), count);
-
-            if (i >= (B - 1)) {
-                HashMap<Integer, Integer> oldMap = null;
-                if (i >= B) {
-                    oldMap = mapList.get(i % B);
-                }
-                int disCount = 0;
-                for (Map.Entry < Integer, Integer > set: currMap.entrySet()) {
-                    int setCount = set.getValue();
-                    if (oldMap != null && oldMap.containsKey(set.getKey())) {
-                        setCount -= oldMap.get(set.getKey());
-                    }
-                    if (setCount > 0) {
-                        disCount++;
-                    }
-                }
-                result.add(disCount);
-                if (oldMap != null) {
-                    oldMap.clear();
-                }
-            }
             
             if (i >= B) {
-                mapList.set((i % B), (HashMap)currMap.clone());    
-            } else {
-                mapList.add((HashMap)currMap.clone());    
+                if (memMap.containsKey(A.get(i - B))) {
+                    int existingCount = memMap.get(A.get(i - B));
+                    if (existingCount == 1) {
+                        memMap.remove(A.get(i - B));
+                    } else {
+                        memMap.put(A.get(i - B), (existingCount - 1));
+                    }
+                }
             }
             
+            int count = 1;
+            if (memMap.containsKey(A.get(i))) {
+                count += memMap.get(A.get(i));
+            }
+            memMap.put(A.get(i), count);
+
+            if (i >= (B - 1)) {
+                result.add(memMap.size());
+            }
         }
         return result;
     }
