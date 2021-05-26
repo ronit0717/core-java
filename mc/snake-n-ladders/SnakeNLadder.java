39d2d04058ec6bdf14a7419cdf42d2cd10b5648a
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-05-26 19:16:43 +0530

    Snake and Ladders .. dynamic changes

--- mc/snake-n-ladders/SnakeNLadder.java
+++ mc/snake-n-ladders/SnakeNLadder.java
@@ -1,19 +1,62 @@
 import model.Game;
 import service.GameSetupService;
 import service.GameplayService;
+import java.util.Scanner;
 
 class SnakeNLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("Snake N Ladder Game started");
 
-		Game game = new Game(100, 8, 7);
+		Scanner sc = new Scanner(System.in);
+		int snakes = -1;
+		int ladders = -1;
+		int players = -1;
+		boolean validInput = false;
+
+		while (!validInput) {
+			System.out.println("\nEnter number of snakes (max = 15, min = 5)... -1 to exit");
+			snakes = sc.nextInt();
+
+			if (snakes == -1) { 
+				System.out.println("\n Exited ... Bye");
+				return; 
+			}
+
+			System.out.println("\nEnter number of ladders (max = 15, min = 5)... -1 to exit");
+			ladders = sc.nextInt();
+
+			if (ladders == -1) { 
+				System.out.println("\n Exited ... Bye");
+				return; 
+			}
+
+			System.out.println("\nEnter number of players (max = 9, min = 2)... -1 to exit");
+			players = sc.nextInt();
+
+			if (players == -1) { 
+				System.out.println("\n Exited ... Bye");
+				return; 
+			}
+
+			if (snakes <= 15 && snakes >= 5 && ladders <= 15 && ladders >= 5 && players <= 9 && players >= 2) {
+				validInput = true;
+			}
+			if (!validInput) {
+				System.out.println("\n Invalid Input... retry");
+
+			}
+		}
+
+		sc.close();
+
+		Game game = new Game(100, snakes, ladders);
 		
 		GameSetupService gameSetupService = new GameSetupService();
 		gameSetupService.setupGame(game);		
 
 		GameplayService gameplayService = new GameplayService();
-		gameplayService.playGame(4, game);
+		gameplayService.playGame(players, game);
 		
 		System.out.println("\nSnake N Ladder Game ends");
 	}
--- mc/snake-n-ladders/service/GameplayService.java
+++ mc/snake-n-ladders/service/GameplayService.java
@@ -12,20 +12,26 @@ public class GameplayService {
 		int[] pos = new int[playerCount];
 		Arrays.fill(pos, -1);
 
-		Integer winner = null;
+		int winnerCount = 0;
+		int[] winners = new int[playerCount];
 
 		GameCell[] cells = game.getGameCells();
 
-		int player = 3;
+		int player = playerCount - 1;
 		int counter = 0;
 		boolean repeat = false;
-		while (winner == null && counter <= 1000) {
+		while ((winnerCount < (playerCount - 1)) && counter <= 1000) {
 			player = (++player) % playerCount;
 			if (player == 0 && !repeat) {
 				System.out.println();
 			}
 			repeat = false;
 			counter++;
+			
+			if (pos[player] == 99) { //player already won
+				continue;
+			}			
+
 			int dice = rollDice();
 			System.out.print("\nPlayer " + (player + 1) + " rolled dice... " + dice);
 
@@ -37,8 +43,9 @@ public class GameplayService {
 			int newPos = pos[player] + dice;
 			System.out.print("\t=> Player "+ (player + 1) + " moved to position: " + (newPos + 1));
 			if (newPos == 99) {
-				winner = player + 1;
+				winners[winnerCount] = player + 1;
 				pos[player] = newPos;
+				winnerCount++;
 				continue;
 			} else if (newPos > 99) {
 				System.out.print("\t=> No move");
@@ -58,12 +65,34 @@ public class GameplayService {
 
 		}
 
-		if (winner != null) {
-			System.out.println("\n\n****************************\nPlayer " + winner + " is the winner!!!\n*********************************");
+		setLooser(winners, playerCount);
+		winnerCount++;
+
+		if (winnerCount == playerCount) {
+			System.out.println("\n******************************************");
+
+			for (int i = 1; i <= winnerCount; i++) {
+				if (i == winnerCount) {
+					System.out.println("Looser   =>\tPlayer "+winners[i-1]);
+					break;
+				}
+				System.out.println("Winner "+i + " =>\tPlayer "+winners[i-1]);
+			}
+
+			System.out.println("******************************************\n");
 		}
 
 		System.out.println("\nGame Play finished for "+ playerCount + " players in " + counter + " counts");
 
+	}
+
+	private void setLooser(int[] winners, int playerCount) {
+		int totalSum = (playerCount * (playerCount + 1)) / 2;
+		int i = 0;
+		for (i = 0; i < playerCount - 1; i++) {
+			totalSum -= winners[i];
+		}
+		winners[i] = totalSum;
 	}
 
 	private int rollDice() {
