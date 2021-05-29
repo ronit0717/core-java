8a25249a2b0ae4e0e6d6e9506f7196565dc97092
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-05-29 20:12:46 +0530

    Missing and repeating numbers: XOR solution

--- gfg/FindMissingAndRepeating.java
+++ gfg/FindMissingAndRepeating.java
@@ -92,3 +92,125 @@ x = (yxSum - yxDiff)/2;
 
 
 */
+
+
+
+//Solution 3 : XOR Solution
+import java.util.ArrayList;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        int arr[] = { 9, 10, 8, 6, 1, 3, 2, 5, 6, 4}; //6 is repeating and 7 is missing
+        int n = 10;
+        
+        //step 1
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ arr[i];
+        }
+        
+        //step 2
+        for (int i = 1; i <= n; i++) {
+            xor = xor ^ i;
+        }
+        
+        //xor = missing number ^ repeating number
+        int num = new Integer(xor);
+        int sigIndex = 1; //right most significant digit in binary representation
+        while (num != 1) {
+            num /= 2;
+            sigIndex++;
+        }
+        
+        ArrayList<Integer> bucket1 = new ArrayList<>();
+        ArrayList<Integer> bucket2 = new ArrayList<>();
+        
+        //step 3 : filling the buckets with numbers in the input array
+        for (int i = 0; i < n; i++) {
+            bucketInsert(arr[i], sigIndex, bucket1, bucket2);
+        }
+        
+        //step 4: filling the buckets with numbers from range [1..n]
+        for (int i = 1; i <= n; i++) {
+            bucketInsert(i, sigIndex, bucket1, bucket2);
+        }
+        
+        //step 5
+        int num1 = 0;
+        for (int i = 0; i < bucket1.size(); i++) {
+            num1 = num1 ^ bucket1.get(i);
+        }
+        
+        int num2 = 0;
+        for (int i = 0; i < bucket2.size(); i++) {
+            num2 = num2 ^ bucket2.get(i);
+        }
+        
+        //step 6
+        int[] res = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num1) {
+                res[0] = num1; //repeating number
+                res[1] = num2; //missing number
+                break;
+            } else if (arr[i] == num2) {
+                res[0] = num2; //repeating number
+                res[1] = num1; //missing number
+            }
+        }
+        
+        print(res);
+        
+     }
+     
+     private static void print(int[] res) {
+         for (int i : res) {
+             System.out.print(i + " ");
+         }
+     }
+     
+     private static void bucketInsert(int i, int sigIndex, ArrayList<Integer> bucket1, ArrayList<Integer> bucket2) {
+        int num = new Integer(i);
+        int count = 1;
+        boolean added = false;
+        
+        while (num != 1) {
+            
+            int rem = num % 2;
+            num /= 2;
+            if (count == sigIndex && rem == 1) {
+                bucket1.add(i);
+                added = true;
+            } else if (count == sigIndex && rem == 0) {
+                bucket2.add(i);
+                added = true;
+            }
+            
+            count++;
+            if (count > sigIndex) {
+                break;
+            }
+        }
+        
+        if (!added) {
+            if (count == sigIndex) {
+                bucket1.add(i);
+            } else {
+                bucket2.add(i);   
+            }
+        }
+    }
+}
+/* Algo
+
+Step 1. Do a xor or all the elements in the array
+Step 2. With the result in step 1, do a xor of all elements from [1..n] => The result, resXor = MisingNumber^RepeatingNumber
+(The resXor right most bit will be 1, this means one of the missing number's and repeating number's bit at that index will be 1 and the other will be 0)
+Step 3. Separate elements of the array in two buckets, one which has that bit at that index as 0, and other which has bit at that index as 1
+Step 4. To the same bucket, now for all elements from [1..n], sort them in the two buckets with the same condition
+Step 5. Do xor in each bucket, we will find the two numbers
+Step 6. If we iterate the array again we will get confirmed which one of these two numbers is the missing number and which one is the repeating number
+
+*/
