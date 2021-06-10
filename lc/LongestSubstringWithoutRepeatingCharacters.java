09bd31b7e701b1bf7d114978d90402ea66449c34
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-06-11 04:09:03 +0530

    Code cleaned

--- lc/LongestSubstringWithoutRepeatingCharacters.java
+++ lc/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,3 +1,27 @@
+//Cleaner solution TC = O(N)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLen = 1;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c) && map.get(c) >= start) {
+                int len = i - start;
+                maxLen = Math.max(len, maxLen);
+                start = map.get(c) + 1;
+            }
+            map.put(c, i);
+        }
+        int len = s.length() - start;
+        maxLen = Math.max(len, maxLen);
+        return maxLen;
+    }
+}
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
     	HashMap<Character, Integer> mySet = new HashMap<Character, Integer>();
