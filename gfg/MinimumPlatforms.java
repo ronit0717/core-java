2d34db9721d3952973274951b30fe78ec9a06e97
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-06-20 05:07:18 +0530

    GFG Minimum Platforms

---
+++ gfg/MinimumPlatforms.java
@@ -0,0 +1,89 @@
+//https://www.geeksforgeeks.org/minimum-number-platforms-required-railwaybus-station/
+
+//Solution 2: TC = O(NLogN), SC = O(1)
+/*
+Sort both arrival and departure arrays
+Iterate the arrival array, now if the arrival[i] <= departure[j], this implies that
+there is a new arrival at that time, hence the number of platforms in use needs to be increased.
+Else, it means that there is a departure happening hence the platform count needs to be decreased.
+
+So we need to find the max number of platforms in use at a given time
+*/
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int j = 0;
+        int maxPlatform = 0; //stores max of currPlatfrom in use at a given time
+        int currPlatform = 0; //stores number of platforms currently in use
+        for (int i = 0; i < n; i++) {
+            while (dep[j] < arr[i]) { //this means some departures happened at this point of time
+                currPlatform--;
+                j++;
+            }
+            currPlatform++; //indicates an arrival
+            maxPlatform = Math.max(maxPlatform, currPlatform);
+        }
+        
+        return maxPlatform;
+    }
+    
+}
+
+
+//Solution 1: TC = O(NLogN), SC = O(N)
+/*
+Algo involves first sorting the array in ascending order of arrival times
+Maintain a PriorityQueue (PQ), which holds the departure times, as we iterate the arrival times
+*/
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        
+        if (n == 0) {
+            return 0;
+        }
+        
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        for( int i = 0; i < n; i++ ) {
+            List<Integer> indexes;
+            if (map.containsKey(arr[i])) {
+                indexes = map.get(arr[i]);
+            } else {
+                indexes = new LinkedList<>();
+            }
+            indexes.add(i);
+            map.put(arr[i], indexes);
+        }
+        //the tree map will ensure, we get all the indexes in sorted manner
+        //index of early arrival will be first
+        
+        PriorityQueue<Integer> deps = new PriorityQueue<>(); //departure times in natural sorted order
+        
+        for (Map.Entry<Integer, List<Integer>> set : map.entrySet()) {
+            List<Integer> indexes = set.getValue();
+            for (Integer index : indexes) {
+                for (Integer d : deps) {
+                    if (arr[index] > d) {
+                        deps.remove(d);
+                        break;
+                    }
+                }
+                deps.add(dep[index]);    
+            }
+        }
+        
+        return deps.size();
+    }
+    
+}
--- ib/greedy/MeetingRooms.java
+++ ib/greedy/MeetingRooms.java
@@ -1,5 +1,37 @@
 //Link: https://www.interviewbit.com/problems/meeting-rooms/
+//Similar to minimum number of platforms question
 
+//Solution 1 -> TC = O(NLogN), SC = O(1)
+public class Solution {
+    public int solve(ArrayList<ArrayList<Integer>> A) {
+        int[] start = new int[A.size()];
+        int[] end = new int[A.size()];
+
+        for (int i = 0; i < A.size(); i++) {
+            start[i] = A.get(i).get(0);
+            end[i] = A.get(i).get(1);
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int currCount = 0; //stores the curr meeting rooms in use count at a given point of time
+        int maxCount = 0; //stores max currCount at a given point of time
+        int j = 0;
+        for (int i = 0; i < A.size(); i++) {
+            while (end[j] <= start[i]) {
+                currCount--;
+                j++;
+            }
+            currCount++;
+            maxCount = Math.max(maxCount, currCount);
+        }
+        return maxCount;
+    }
+}
+
+
+//Solution 2: Using a minHeap
 //To find the minimum number of meeting rooms, TC = O(NLogN), SC = O(N)
 /*
 1. Order the events in increasing order of start time
