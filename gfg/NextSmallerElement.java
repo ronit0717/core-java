35a620835b55772f23cc6f4c942928cdaf122e27
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-07-20 06:56:03 +0530

    Next Greater Element And Next Smaller Element

---
+++ gfg/NextSmallerElement.java
@@ -0,0 +1,47 @@
+//https://www.geeksforgeeks.org/next-smaller-element/
+
+/*
+1. Iterate in reverse
+    1.1 Compare the current element with top elements in stack
+    1.2 If the stack number is greater than or equal to current element remove it
+        Else, this is the next smaller element
+    1.3 If we dont find any next smaller element assign -1 to next smaller element array
+*/
+
+import java.util.Stack;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        //int[] nums = { 4, 8, 5, 2, 25 }; 
+        int[] nums = { 13, 7, 6, 12 };
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[nums.length];
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            boolean done = false;
+            while (!st.isEmpty()) {
+                if (st.peek() < nums[i]) {
+                    res[i] = st.peek();
+                    done = true;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+            if (!done) {
+                res[i] = -1;
+            }
+            st.push(nums[i]);
+        }
+        
+        print(res);
+     }
+     
+     private static void print(int[] nums) {
+         for (int i = 0; i < nums.length; i++) {
+             System.out.print(nums[i] + " ");
+         }
+         System.out.println();
+     }
+}
---
+++ lc/NextGreaterElementII.java
@@ -0,0 +1,38 @@
+/*
+Solution:
+
+Steps:
+1. Double the array, to make it circular
+2. Iterate in reverse
+    2.1 Compare the current element with elements in stack
+    2.2 If the stack number is less than or equal to current element remove it
+        Else, this is the next greater element
+    2.3 If we dont find any next greater element assign -1 to next greater element array
+*/
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        
+        //Double loop of nums array
+        for (int i = (2*n - 1); i >= 0; i--) {
+            int curr = nums[i % n];
+            boolean done = false;
+            while (!st.isEmpty()) {
+                if (curr < st.peek()) {
+                    res[i % n] = st.peek();
+                    done = true;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+            if (!done) {
+                res[i % n] = -1;
+            }
+            st.push(curr);
+        }
+        return res;
+    }
+}
