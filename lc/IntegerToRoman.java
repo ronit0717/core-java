f1e3537b513a0ed33a28c4fa3919d7946d3ca170
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-07-25 11:44:37 +0530

    Integer to roman and vice versa

---
+++ lc/IntegerToRoman.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        
+        int[] nums = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+        String[] romans = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+        
+        while (num > 0) {
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (num < nums[i]) {
+                    continue;
+                }
+                int quotient = num / nums[i];
+                num = num % nums[i];
+                
+                for (int j = 0; j < quotient; j++) {
+                    sb.append(romans[i]);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ lc/RomanToInteger.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/roman-to-integer/
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ( (i < (s.length() - 1)) && ( map.get(s.charAt(i + 1)) > map.get(s.charAt(i)) ) )  {
+                val += ( map.get(s.charAt(i + 1)) - map.get(s.charAt(i))  );
+                i++;
+            } else {
+                val += map.get(s.charAt(i));
+            }
+        }
+        return val;
+    }
+}
