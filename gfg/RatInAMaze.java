53605d967258e66ee8f5f107c15456b27a984250
ronit0717 <ronit0717@gmail.com>
2024-08-02 13:10:15 +0530

    Rat in a maze

--- gfg/RatInAMaze.java
+++ gfg/RatInAMaze.java
@@ -1,5 +1,65 @@
 //https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
 
+/* Solution 2: Without changing the original maze. Using a visited array */
+class Solution {
+    public ArrayList<String> findPath(int[][] mat) {
+        // Your code here
+        char[] dirCode = {'D', 'L', 'R', 'U'}; //lexicographic order
+        int[][] dirArr = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+        ArrayList<String> paths = new ArrayList<>();
+        if (mat[0][0] == 0) {
+            return paths;
+        }
+        LinkedList<Character> candidates = new LinkedList<>();
+        int N = mat.length;
+        boolean[] visited = new boolean[N * N];
+        visited[0] = true; //starting position
+        evaluate(mat, 0, visited, candidates, paths, dirCode, dirArr);
+        return paths;
+    }
+    
+    private void evaluate(int[][]mat, 
+                            int index, 
+                            boolean[] visited, 
+                            LinkedList<Character> candidates, 
+                            ArrayList<String> paths,
+                            char[] dirCode,
+                            int[][] dirArr) {
+        int N = mat.length;
+        if (index == (N * N - 1)) { //destination reached
+            paths.add(buildPath(candidates));
+            return;
+        }
+        int currRow = index / N;
+        int currCol = index % N;
+        for (int i = 0; i < 4; i++) {
+            int newRow = currRow + dirArr[i][0];
+            int newCol = currCol + dirArr[i][1];
+            if (newRow < 0 || newRow >= N || newCol < 0 || newCol >= N) {
+                continue; //out of maze
+            }
+            int newIndex = newRow * N + newCol;
+            if (mat[newRow][newCol] == 0 || visited[newIndex]) {
+                continue; //invalid move
+            }
+            visited[newIndex] = true;
+            candidates.add(dirCode[i]);
+            evaluate(mat, newIndex, visited, candidates, paths, dirCode, dirArr);
+            
+            visited[newIndex] = false; //backtrack
+            candidates.removeLast();
+        }
+    }
+    
+    private String buildPath(List<Character> candidates) {
+        StringBuffer sb = new StringBuffer();
+        for (Character c : candidates) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
+
 /*
 Simple backtracking logic
 
