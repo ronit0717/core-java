cd65e96ccd9d5a4f3df25afc34552d00e5c66068
ronit0717 <ronit0717@gmail.com>
2024-08-05 19:43:44 +0530

    K Jump

--- cn/FrogJump.java
+++ cn/FrogJump.java
@@ -1,3 +1,48 @@
+/** Solution 1.4: K Jump (Here k = 2) */
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+        return evaluate(n - 1, heights);
+    }
+
+    private static int evaluate(int index, int[] heights) {
+        if (index == 0) {
+            return 0;
+        }
+        int k = 2; //2 steps allowed
+        List<Integer> prev = new ArrayList<>(k);
+        for (int i = 0; i <= index; i++) {
+            if (i == 0) {
+                prev.add(0);
+                continue;
+            }
+            int sum = Integer.MAX_VALUE;
+            for (int j = 1; j <= k; j++) {
+                if ((i - j) < 0) {
+                    break;
+                }
+                int currSum = prev.get(prev.size() - j) + computeEnergy(i, i - j, heights);
+                sum = Math.min(sum, currSum);
+            }
+            if (prev.size() == k) {
+                prev.remove(0);
+                prev.add(sum);
+            } else {
+                prev.add(sum);
+            }
+        }
+        return prev.get(prev.size() - 1); //last
+    }
+
+    private static int computeEnergy(int pos1, int pos2, int[] heights) {
+        return Math.abs(heights[pos1] - heights[pos2]);
+    }
+
+}
+
 /** Solution 1.3: Tabulation space optimised */
 import java.util.* ;
 import java.io.*; 
