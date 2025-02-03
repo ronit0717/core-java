1cef42013a624f1cc344a7f2530fb4170e858383
ronit0717 <ronit0717@gmail.com>
2025-02-03 11:11:39 +0530

    More compact solution

--- lc/BestTimeToBuyAndSellStock.java
+++ lc/BestTimeToBuyAndSellStock.java
@@ -1,3 +1,19 @@
+/*
+Solution 3 (Same approach as Solution 2, but more compact code)
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return profit;
+    }
+}
+
+
 //Soltion 2 TC = O(n),  using only minima
 /*
 1. Set first element as minima, and set profit = 0
