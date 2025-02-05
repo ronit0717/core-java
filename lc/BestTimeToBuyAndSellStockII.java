7ac4e6443d380188e52cdfbe3ec3a0829d88d2b9
ronit0717 <ronit0717@gmail.com>
2025-02-05 12:31:04 +0530

    Best time to buy and sell stock II - Tabulation

--- lc/BestTimeToBuyAndSellStockII.java
+++ lc/BestTimeToBuyAndSellStockII.java
@@ -1,4 +1,50 @@
 /**
+ * Solution 3: Tabulation space optimised 1DP DP
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[] dp = new int[2];
+        int[] prev = new int[2];
+        for (int i = prices.length; i >= 0 ; i--) {
+            for (int k = 0; k <= 1; k++) {
+                prev[k] = dp[k];
+            }
+            for (int j = 1; j >= 0; j--) {
+                if (i == prices.length) {
+                    dp[j] = 0;
+                    continue;
+                }
+                int choice1 = prices[i] * (j == 0 ? 1 : -1) + prev[j == 0 ? 1 : 0];
+                int choice2 = prev[j]; //skip case
+                dp[j] = Math.max(choice1, choice2);
+            }
+        }
+        return dp[1];
+    }
+}
+
+/**
+ * Solution 2: Tabulation with 2D DP
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+        for (int i = prices.length; i >= 0 ; i--) {
+            for (int j = 1; j >= 0; j--) {
+                if (i == prices.length) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int choice1 = prices[i] * (j == 0 ? 1 : -1) + dp[i + 1][j == 0 ? 1 : 0];
+                int choice2 = dp[i + 1][j]; //skip case
+                dp[i][j] = Math.max(choice1, choice2);
+            }
+        }
+        return dp[0][1];
+    }
+}
+
+/**
  * Solution 1: Memoisation
  */
 class Solution {
