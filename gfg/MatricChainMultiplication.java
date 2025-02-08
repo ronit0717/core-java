bbe92b8d52fb4d16a2aca4343d3fbd689144499b
ronit0717 <ronit0717@gmail.com>
2025-02-08 15:20:23 +0530

    MCM - Tabulation

--- gfg/MatricChainMultiplication.java
+++ gfg/MatricChainMultiplication.java
@@ -1,5 +1,36 @@
 /**
+ * Solution 2: Tabulation
+ * TC = O(N^3), SC=O(N^2)
+ * 
+ * Further space optimisation not possible.. as we need dp[k + 1][j] value
+ */
+class Solution {
+    static int matrixMultiplication(int arr[]) {
+        // code here
+        int[][] dp = new int[arr.length][arr.length];
+        for (int i = arr.length - 1; i >= 1; i--) {
+            for (int j = i; j < arr.length; j++) { //approach smaller sub problem first
+                if (i == j) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int minOps = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int ops = arr[i - 1] * arr[k] * arr[j];
+                    ops += dp[i][k];
+                    ops += dp[k + 1][j];
+                    minOps = Math.min(ops, minOps);
+                }
+                dp[i][j] = minOps;
+            }
+        }
+        return dp[1][arr.length - 1];
+    }
+}
+
+/**
  * Solution 1: Memoisation
+ * TC = O(N^3), SC=O(N^2) + O(N) auxillary space
  */
 class Solution {
     static int matrixMultiplication(int arr[]) {
