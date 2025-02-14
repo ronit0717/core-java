025d029aaf3c76279904e32319a0c4a2e2354ef8
ronit0717 <ronit0717@gmail.com>
2025-02-14 17:23:46 +0530

    Single pass all traversal in iterative manner

--- cn/TreeTraversal.java
+++ cn/TreeTraversal.java
@@ -1,3 +1,80 @@
+//Solution 2: Iterative Solution
+/*********************************************************
+
+ Following is the TreeNode structure:
+
+ class TreeNode {
+     int data;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {
+         this.data = 0;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int data) {
+         this.data = data;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int data, TreeNode left, TreeNode right) {
+         this.data = data;
+         this.left = left;
+         this.right = right;
+     }
+ };
+ ********************************************************/
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution {
+    public static List<List<Integer>> getTreeTraversal(TreeNode root) {
+        // Write your code here.
+        List<Integer> preOrder = new LinkedList<>();
+        List<Integer> inOrder = new LinkedList<>();
+        List<Integer> postOrder = new LinkedList<>();
+        
+        TreeNode node = root;
+        Stack<TreeNode> st = new Stack<>();
+        while(node != null) {
+            preOrder.add(node.data);
+            st.push(node);
+            node = node.left; //go left deep
+        }
+
+        TreeNode prev = null;
+        while(!st.isEmpty()) {
+            node = st.peek();
+            if (node.right == null) { //leaf node
+                postOrder.add(node.data);
+                inOrder.add(node.data);
+                prev = st.pop();
+            } else if (node.right == null || prev == node.right) { //no right or left and right already visited
+                postOrder.add(node.data);
+                prev = st.pop();
+            } else { //need to traverse right
+                inOrder.add(node.data);
+                node = node.right;
+                while (node != null) {
+                    preOrder.add(node.data);
+                    st.push(node);
+                    node = node.left; //go left deep
+                }
+            }
+        }
+        
+        List<List<Integer>> ans = new ArrayList<>(3);
+        ans.add(inOrder);
+        ans.add(preOrder);
+        ans.add(postOrder);
+        return ans;
+    }
+}
+
+//Solution 1: Recursive Solution
 /*********************************************************
 
  Following is the TreeNode structure:
