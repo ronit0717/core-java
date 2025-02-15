d7b9dbed1764685fa6016362d597fcaec67fb24d
ronit0717 <ronit0717@gmail.com>
2025-02-15 11:57:30 +0530

    Binary tree height Level order traversal solution

--- lc/MaximumDepthOfBinaryTree.java
+++ lc/MaximumDepthOfBinaryTree.java
@@ -13,6 +13,34 @@
  *     }
  * }
  */
+//Solution 2 (Slower) : Iterative (Level order traversing)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int height = 0;
+        while(!q.isEmpty()) {
+            height++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+        return height;
+    }
+}
+
+
+//Solution 1 : Recursive Solution
 class Solution {
     public int maxDepth(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
