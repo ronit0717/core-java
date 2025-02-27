6a08059f2b56577cd15319e22d21308382660e68
ronit0717 <ronit0717@gmail.com>
2025-02-28 00:31:11 +0530

    Maximum width of a binary tree

--- lc/MaximumWidthOfBinaryTree.java
+++ lc/MaximumWidthOfBinaryTree.java
@@ -1,10 +1,10 @@
 /*
 Approach:
-Assign root node as level 0
-Left Node will have level = 2(rootLevel) = 2(0) = 0
-Right Node will have level = 2(rootLevel) + 1 = 2(0) +1 = 1
+Assign root node as index =  0
+Left Node will have index = 2(rootIndex) = 2(0) = 0
+Right Node will have index = 2(rootIndex) + 1 = 2(0) +1 = 1
 
-Width each level = (Level of the right most node in each level - Level of the left most node in each level)
+Width each level = (Index of the right most node in each level - Index of the left most node in each level) + 1
 */
 
 /**
@@ -24,50 +24,38 @@ Width each level = (Level of the right most node in each level - Level of the le
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        Queue<Object[]> q = new LinkedList<>();
-        
-        q.add(new Object[]{root, 0});
-        q.add(null);
-        
         int maxWidth = 0;
-        
-        int start = 0;
-        
-        while (!q.isEmpty()) {
-            Object[] obj = q.poll();
-            
-            if (obj == null && q.isEmpty()) {
-                break;
-            } else if (obj == null) {
-                q.add(null);
-                start = (int)q.peek()[1];
-                continue;
+        Queue<Object[]> q = new LinkedList<>();
+        Object[] qNode = {root, 0};
+        q.add(qNode);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            int minIndex = 0;
+            int maxIndex = 0;
+            for (int i = 0; i < size; i++) {
+                qNode = q.poll();
+                TreeNode node = (TreeNode)qNode[0];
+                int index = (int)qNode[1];
+                if (i == 0) {
+                    minIndex = index;
+                }
+                if (i == (size - 1)) {
+                    maxIndex = index;
+                }
+                if (node.left != null) {
+                    int leftIndex = 2 * index;
+                    Object[] leftQNode = {node.left, leftIndex};
+                    q.add(leftQNode);
+                }
+                if (node.right != null) {
+                    int rightIndex = 2 * index + 1;
+                    Object[] rightQNode = {node.right, rightIndex};
+                    q.add(rightQNode);
+                }
             }
-            
-            TreeNode curr = (TreeNode)obj[0];
-            int currLevel = (int)obj[1];
-            
-            if (q.peek() == null) { //level ends
-                int currLevelWidth = currLevel - start + 1;
-                maxWidth = Math.max(maxWidth, currLevelWidth);
-            }
-            
-            if (curr.left != null) {
-                q.add(new Object[]{ curr.left,  currLevel*2});
-            }
-            
-            if (curr.right != null) {
-                q.add(new Object[]{ curr.right,  currLevel*2 + 1});
-            }
-            
-            
+            int width = maxIndex - minIndex + 1;
+            maxWidth = Math.max(maxWidth, width);
         }
-        
         return maxWidth;
     }
-    
-    
 }
