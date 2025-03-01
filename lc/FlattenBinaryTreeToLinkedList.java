de94d7860fc279cb9bc12df78f4788a493dcd777
ronit0717 <ronit0717@gmail.com>
2025-03-01 18:57:22 +0530

    Flatten binary tree to linkedlist

--- lc/FlattenBinaryTreeToLinkedList.java
+++ lc/FlattenBinaryTreeToLinkedList.java
@@ -13,27 +13,64 @@
  *     }
  * }
  */
+//Solution 3: Morris Traversal (TC = O(N), SC = O(1))
 class Solution {
     public void flatten(TreeNode root) {
+        TreeNode node = root;
+        while(node != null) {
+            if (node.left == null) {
+                node = node.right;
+                continue;
+            }
+            TreeNode target = node.left;
+            while(target.right != null) {
+                target = target.right;
+            }
+            target.right = node.right;
+            node.right = node.left;
+            node.left = null;
+            node = node.right;
+        }
+    }
+}
+
+//Solution 2: Iterative approach using stack (TC = O(n), SC = O(LogN) for the stack)
+class Solution {
+    public void flatten(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
         if (root == null) {
             return;
         }
-        
-        //flatten each child
-        flatten(root.left);
-        flatten(root.right);
-        
-        //swap
-        TreeNode temp = root.right;
-        root.right = root.left;
-        root.left = null;
-        
-        //treverse right till end
-        TreeNode node = root;
-        while (node.right != null) {
-            node = node.right;
+        st.push(root);
+        while(!st.isEmpty()) {
+            TreeNode node = st.pop();
+            if (node.right != null) {
+                st.push(node.right);
+            }
+            if (node.left != null) {
+                st.push(node.left);
+            }
+            node.right = st.isEmpty() ? null : st.peek();
+            node.left = null;
         }
-        
-        node.right = temp;
+    }
+}
+
+//Solution 1: Recursive Approach (TC = O(N), SC = O(LogN) for auxillary stack stace)
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode prev = null;
+        flatUtil(root, prev);
+    }
+
+    private TreeNode flatUtil(TreeNode root, TreeNode prev) {
+        if (root == null) {
+            return prev;
+        }
+        prev = flatUtil(root.right, prev);
+        prev = flatUtil(root.left, prev);
+        root.right = prev;
+        root.left = null;
+        return root;
     }
 }
