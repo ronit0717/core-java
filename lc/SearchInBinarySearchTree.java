2c72ce88673507e72ac0e3e5f2c822fbe821d784
ronit0717 <ronit0717@gmail.com>
2025-03-03 23:06:28 +0530

    Search in binary search tree

---
+++ lc/SearchInBinarySearchTree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//Solution 2: Iterative approach
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode node = root;
+        while (node != null && node.val != val) {
+            node = node.val < val ? node.right : node.left;
+        }
+        return node;
+    }
+}
+
+//Solution 1: Recursive approach
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
