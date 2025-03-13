e8ae7050996a31d05e78dcfbb41c9e6d6d21e129
ronit0717 <ronit0717@gmail.com>
2025-03-13 12:49:55 +0530

    TC corrected

--- gfg/KeyPair.java
+++ gfg/KeyPair.java
@@ -1,7 +1,7 @@
 //Problem link: https://www.geeksforgeeks.org/problems/key-pair5616/1
 
 //Two sum extension. Uses greedy approach post sorting the array
-//TC = O(N), SC = O(1)
+//TC = O(N) + O(NLogN), SC = O(1)
 class Solution {
     boolean twoSum(int arr[], int target) {
         // code here
