9209d5956769de66aaebc4f3815973185399c671
ronit0717 <ronit0717@gmail.com>
2025-03-13 16:20:17 +0530

    Recover BST

---
+++ lc/RecoverBinarySearchTree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//TC = O(N) + O(N), SC = O(Height) Auxillary stack space + O(N) for ArrayList
+class Solution {
+    public void recoverTree(TreeNode root) {
+        List<TreeNode> inorder = new ArrayList<>();
+        buildInorder(root, inorder);
+        sortCheckAndSwap(inorder);
+    }
+
+    private void sortCheckAndSwap(List<TreeNode> inorder) {
+        int index1 = -1;
+        int index2 = -1;
+        for (int i = 1; i < inorder.size(); i++) {
+            if (inorder.get(i).val < inorder.get(i - 1).val) {
+                if (index1 == -1) {
+                    index1 = i;
+                } else if (index2 == -1) {
+                    index2= i;
+                }
+            }
+        }
+        if (index2 == -1) {
+            //adjacent case. Swap index1-1 and index1
+            int temp = inorder.get(index1 - 1).val;
+            inorder.get(index1 - 1).val = inorder.get(index1).val;
+            inorder.get(index1).val = temp;;
+        } else {
+            //swap index1 - 1 and index2
+            int temp = inorder.get(index1 - 1).val;
+            inorder.get(index1 - 1).val = inorder.get(index2).val;
+            inorder.get(index2).val = temp;
+        }
+    }
+
+    private void buildInorder(TreeNode node, List<TreeNode> inorder) {
+        if (node == null) {
+            return;
+        }
+        buildInorder(node.left, inorder);
+        inorder.add(node);
+        buildInorder(node.right, inorder);
+    }
+}
