ef02668a50cbbd86bac515645e7badacaedbebfb
ronit0717 <ronit0717@gmail.com>
2025-03-16 18:44:01 +0530

    Graph bipartite solution improved

--- lc/IsGraphBipartite.java
+++ lc/IsGraphBipartite.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int[] visited = new int[graph.length]; //0 means not visited, 1 represent first color, -1 represent other color
-        
+        int[] visited = new int[graph.length];
         for (int i = 0; i < graph.length; i++) {
             if (visited[i] != 0) {
                 continue;
             }
-            boolean status = dfsUtil(i, -1, graph, visited, 1);
+            boolean status = util(i, 1, visited, graph);
             if (!status) {
-                return status;
+                return false;
             }
         }
         return true;
     }
-    
-    private static boolean dfsUtil(int curr, int prev, int[][] graph, int[] visited, int color) {
-        if (visited[curr] != 0) {
-            if (color != visited[curr]) {
-                return false;
-            } else {
-                return true;
-            }
+
+    private boolean util(int index, int color, int[] visited, int[][] graph) {
+        if (visited[index] != 0 && color == visited[index]) {
+            return true;
+        } else if (visited[index] != 0 && color != visited[index]) {
+            return false;
         }
-        visited[curr] = color;
-        int[] next = graph[curr];
-        for (Integer n : next) {
-            if (n == prev) {
-                continue;
-            }
-            boolean status = dfsUtil(n, curr, graph, visited, (color*-1));
+        visited[index] = color;
+        int[] nextArr = graph[index];
+        for (int i = 0; i < nextArr.length; i++) {
+            int newColor = (color == 1) ? -1 : 1;
+            boolean status = util(nextArr[i], newColor, visited, graph);
             if (!status) {
-                return status;
+                return false;
             }
         }
         return true;
