a322d0cb482635844ee51ca1fdff6777ec1dfe33
ronit0717 <ronit0717@gmail.com>
2025-03-17 10:58:41 +0530

    Kahn's Algo

--- gfg/TopologicalSort.java
+++ gfg/TopologicalSort.java
@@ -1,3 +1,49 @@
+//Solution 2: BFS (Kahn's Algorithm)
+class Solution {
+    // Function to return list containing vertices in Topological order.
+    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        // Your code here
+        int[] inDegree = buildInDegree(adj);
+        ArrayList<Integer> topo = new ArrayList<>(adj.size());
+        Queue<Integer> q = new LinkedList<>();
+        populateTopoAndQueue(inDegree, topo, q, adj);
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            for (int next : adj.get(node)) {
+                inDegree[next] -= 1;
+                if (inDegree[next] == 0) {
+                    q.add(next);
+                    topo.add(next);
+                }
+            }
+        }
+        return topo;
+    }
+    
+    private static void populateTopoAndQueue(int[] inDegree,
+                                ArrayList<Integer> topo, 
+                                Queue<Integer> q, 
+                                ArrayList<ArrayList<Integer>> adj) {
+        for (int i = 0; i < adj.size(); i++) {
+            if(inDegree[i] == 0) {
+                topo.add(i);
+                q.add(i);
+            }
+        }
+    }
+    
+    private static int[] buildInDegree(ArrayList<ArrayList<Integer>> adj) {
+        int[] inDegree = new int[adj.size()];
+        for (int i = 0; i < adj.size(); i++) {
+            for (int next : adj.get(i)) {
+                inDegree[next] += 1;
+            }
+        }
+        return inDegree;
+    }
+}
+
+//Solution 1: DFS
 class Solution {
     // Function to return list containing vertices in Topological order.
     static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
