e258e8cead7bc0993832c1ac1f9579903f3c4108
ronit0717 <ronit0717@gmail.com>
2025-03-29 23:11:27 +0530

    Kruskal's Algorithm

--- gfg/MinimumSpanningTree.java
+++ gfg/MinimumSpanningTree.java
@@ -1,3 +1,4 @@
+//Solution 1: Prim's Algorithm
 class Solution {
     static int spanningTree(int V, int E, List<List<int[]>> adj) {
         // Code Here.
@@ -25,5 +26,102 @@ class Solution {
             }
         }
         return weight;
+    }
+}
+
+//Solution 2: Kruskal's Algorithm
+class Edge implements Comparable<Edge> {
+    int start;
+    int end;
+    int weight;
+    
+    public Edge(int start, int end, int weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+    
+    public int compareTo(Edge e) {
+        return this.weight - e.weight;
+    }
+    
+}
+
+class DisjointSet {
+    int[] parent;
+    int[] size;
+    
+    public DisjointSet(int n) {
+        this.parent = new int[n];
+        this.size = new int[n];
+        for (int i = 0; i < n; i++) {
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+    
+    private int getUltimateParent(int x) {
+        if (this.parent[x] == x) {
+            return x;
+        }
+        int ultimateParent = getUltimateParent(this.parent[x]);
+        this.parent[x] = ultimateParent;
+        return ultimateParent;
+    }
+    
+    public void union(int x, int y) {
+        int ultimateParentX = getUltimateParent(x);
+        int ultimateParentY = getUltimateParent(y);
+        if (ultimateParentX == ultimateParentY) {
+            return;
+        }
+        
+        if (this.size[ultimateParentX] < this.size[ultimateParentY]) {
+            this.parent[ultimateParentX] = ultimateParentY;
+            this.size[ultimateParentY] = this.size[ultimateParentX] + this.size[ultimateParentY];
+        } else {
+            this.parent[ultimateParentY] = ultimateParentX;
+            this.size[ultimateParentY] = this.size[ultimateParentX] + this.size[ultimateParentY];
+        }
+    }
+    
+    public boolean isConnected(int x, int y) {
+        int ultimateParentX = getUltimateParent(x);
+        int ultimateParentY = getUltimateParent(y);
+        return ultimateParentX == ultimateParentY;
+    }
+}
+
+class Solution {
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        // Code Here.
+        List<Edge> edges = buildSortedEdgeList(adj);
+        int sizeSum = 0;
+        DisjointSet ds = new DisjointSet(V);
+        for (Edge edge : edges) {
+            int x = edge.start;
+            int y = edge.end;
+            if (ds.isConnected(x, y)) {
+                continue;
+            }
+            ds.union(x, y);
+            sizeSum += edge.weight;
+        }
+        return sizeSum;
+    }
+    
+    private static List<Edge> buildSortedEdgeList(List<List<int[]>> adj) {
+        List<Edge> edgeList = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                int start = i;
+                int end = adj.get(i).get(j)[0];
+                int weight = adj.get(i).get(j)[1];
+                Edge edge = new Edge(start, end, weight);
+                edgeList.add(edge);
+            }
+        }
+        Collections.sort(edgeList);
+        return edgeList;
     }
 }
