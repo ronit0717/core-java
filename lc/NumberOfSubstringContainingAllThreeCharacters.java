b81d454649895e99c42baae4951b5f22a05641b1
ronit0717 <ronit0717@gmail.com>
2025-04-02 01:30:03 +0530

    Cleaner solution added

--- lc/NumberOfSubstringContainingAllThreeCharacters.java
+++ lc/NumberOfSubstringContainingAllThreeCharacters.java
@@ -1,3 +1,35 @@
+//Solution 2 (Cleaner Solution)
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int[] pos = {-1, -1, -1};
+        int count = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            pos[c - 'a'] = j;
+            if (!hasAllThree(pos)) {
+                continue;
+            }
+            int minPos = getStartOfMinSubstring(pos);
+            count += (minPos + 1);
+        }
+        return count;
+    }
+
+    private int getStartOfMinSubstring(int[] pos) {
+        return Math.min(pos[0], Math.min(pos[1], pos[2]));
+    }
+
+    private boolean hasAllThree(int[] pos) {
+        for (int i = 0; i < 3; i++) {
+            if(pos[i] == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//Solution 1
 class Solution {
     public int numberOfSubstrings(String s) {
         int aPos = -1;
