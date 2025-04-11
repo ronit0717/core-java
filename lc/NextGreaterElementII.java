ee69746019a905f8ccd225f7ed75f78e2db9269b
ronit0717 <ronit0717@gmail.com>
2025-04-11 22:30:08 +0530

    Next greater element 2

--- lc/NextGreaterElementII.java
+++ lc/NextGreaterElementII.java
@@ -1,3 +1,22 @@
+//Solution 2 (Cleaner code)
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+            if (i < n) {
+                nge[i] = st.isEmpty() ? -1 : st.peek();
+            }
+            st.push(nums[i % n]);
+        }
+        return nge;
+    }
+}
+
 /*
 Solution:
 
