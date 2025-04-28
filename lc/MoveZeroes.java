4d4cd2be74c93fd3afc4a2e0d9e23ea9f2a17a1c
ronit0717 <ronit0717@gmail.com>
2025-04-28 23:42:05 +0530

    Move zeroes

---
+++ lc/MoveZeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                //shift
+                int temp = nums[j];
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
