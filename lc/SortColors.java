6d230f03b241ea5db7a62e2c165551f6f676ac71
ronit0717 <ronit0717@gmail.com>
2025-04-29 12:07:44 +0530

    Sort color dutch flag algorithm

--- lc/SortColors.java
+++ lc/SortColors.java
@@ -1,3 +1,30 @@
+//Solution 3
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while(mid <= high) {
+            if (nums[mid] == 0) {
+                //swap
+                int temp = nums[low];
+                nums[low] = 0;
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                //swap
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
 //Solution 2 : Cleaner Code
 class Solution {
     public void sortColors(int[] nums) {
