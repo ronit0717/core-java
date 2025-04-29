99a68d0c926a557b8cafc06f73aabd7ee9a13fc2
ronit0717 <ronit0717@gmail.com>
2025-04-29 09:53:20 +0530

    Missing number

--- lc/MissingNumber.java
+++ lc/MissingNumber.java
@@ -1,3 +1,17 @@
+//Solution 2: XOR approach
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+            xor = xor ^ i;
+        }
+        xor = xor ^ nums.length;
+        return xor;
+    }
+} 
+
+//Solution 1: Sum approach
 class Solution {
     public int missingNumber(int[] nums) {
         int len = nums.length;
