b61b21664c4f0a72e7c369d52e4d2b67f8298182
ronit0717 <ronit0717@gmail.com>
2025-05-03 23:08:09 +0530

    Rotate image

--- lc/RotateImage.java
+++ lc/RotateImage.java
@@ -1,25 +1,55 @@
+//Solution 2 : Inplace solution => transpose + reverse row. SC = O(1)
 class Solution {
     public void rotate(int[][] matrix) {
-        int swapLimit = matrix.length/2;
-        
-        //vertical swaps
-        for (int i = 0; i < swapLimit; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+
+    private void reverse(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                //swap
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[matrix.length - 1 - i][j];
-                matrix[matrix.length - 1 - i][j] = temp;
+                matrix[i][j] = matrix[i][m - 1 - j];
+                matrix[i][m - 1 - j] = temp;
             }
         }
-        
-        //diagonal swaps
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (j >= i) {
-                    continue;
-                }
+    }
+
+    private void transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                //swap
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
+            }
+        }
+    }
+}
+
+//Solution 1 (Using a separate matrix)
+/*
+matrix[j][m - 1 - i] = ansMatrix[i][j]
+*/
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        //copy
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = ans[i][j];
             }
         }
     }
