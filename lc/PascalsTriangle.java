c3f2c6cfcaa74b3af8ffc05b08887c281c974ded
ronit0717 <ronit0717@gmail.com>
2025-05-04 10:48:54 +0530

    Pascal's triangle

--- lc/PascalsTriangle.java
+++ lc/PascalsTriangle.java
@@ -1,15 +1,69 @@
+//Solution 3 (Optimised NCR. Using the pattern of multiplication, optimised solution)
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new LinkedList<>();
-        List<Integer> row = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            row.add(0, 1);
-            for (int j = 1; j < row.size() - 1; j++) {
-                row.set(j, row.get(j) + row.get(j + 1));
+        List<List<Integer>> ans = new ArrayList<>(numRows);
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>(i);
+            int num = 1;
+            list.add(num);
+            for (int j = 2; j <= i; j++) {
+                num *= (i - j + 1);
+                num /= (j - 1);
+                list.add(num);
             }
-            result.add(new LinkedList<>(row));
+            ans.add(list);
         }
-        return result;
+        return ans;
+    }
+}
+
+//Solution 2 (Using NCR)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>(numRows);
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>(i);
+            for (int j = 1; j <= i; j++) {
+                int num = ncr(i - 1, j - 1);
+                list.add(num);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+
+    private int ncr(int n, int r) {
+        if (r == 0 || n == r) {
+            return 1;
+        }
+        int ans = 1;
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - i + 1);
+            ans = ans / i;
+        }
+        return ans;
+    }
+}
+
+//Solution 1 (Intuitive solution)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>(numRows);
+        List<Integer> list = null;
+        List<Integer> prev = null;
+        for (int i = 0; i < numRows; i++) {
+            prev = list;
+            list = new ArrayList<>(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    list.add(1);
+                } else {
+                    list.add(prev.get(j) + prev.get(j - 1));
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
     }
 }
 
