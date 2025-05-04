57ec9e1c04b8c49340412fd7ec3d27e4355c80ce
ronit0717 <ronit0717@gmail.com>
2025-05-04 12:42:24 +0530

    Majority element II

--- lc/MajorityElementII.java
+++ lc/MajorityElementII.java
@@ -1,49 +1,54 @@
 //Solution 1: Moore's majority element algorithm TC = O(N), SC = O(1)
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int element1 = -1;
-        int element2 = -1;
         int count1 = 0;
         int count2 = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == element1) {
+        int large = 1000000001;
+        int num1 = large;
+        int num2 = large;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (num1 == nums[i]) {
                 count1++;
-            } else if (nums[i] == element2) {
+            } else if (num2 == nums[i]) {
                 count2++;
             } else if (count1 == 0) {
-                element1 = nums[i];
+                num1 = nums[i];
                 count1++;
             } else if (count2 == 0) {
-                element2 = nums[i];
+                num2 = nums[i];
                 count2++;
             } else {
                 count1--;
                 count2--;
+                //the following reset is actually not required
+                if (count1 == 0) {
+                    num1 = large;
+                }
+                if (count2 == 0) {
+                    num2 = large;
+                }
             }
         }
 
-        List<Integer> res = new LinkedList<>();
-
-        //Get the actual count as no guarantee that the two elements will satisfy condition
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == element1) {
-                count1++;
-            } else if (nums[i] == element2) {
-                count2++;
+        //check
+        List<Integer> ans = new ArrayList<>(2); //there can be max 2 majority elements
+        int actualCount1 = 0;
+        int actualCount2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (num1 != large && nums[i] == num1) {
+                actualCount1++;
+            } else if (num2 != large && nums[i] == num2) {
+                actualCount2++;
             }
         }
-
-        if (count1 > (nums.length/3) ) {
-            res.add(element1);
+        if (actualCount1 > (n / 3)) {
+            ans.add(num1);
         }
-        if (count2 > (nums.length/3) ) {
-            res.add(element2);
+        if (actualCount2 > (n / 3)) {
+            ans.add(num2);
         }
-
-        return res;
+        return ans;
     }
 }
 
