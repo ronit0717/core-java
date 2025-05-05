06bfdbbe837f8e92583e88ab4cc1b092d0d9246b
ronit0717 <ronit0717@gmail.com>
2025-05-05 19:04:40 +0530

    Check min heap

---
+++ td/CheckMinHeap.java
@@ -0,0 +1,58 @@
+/*
+
+Given an integer array, check if it represents min-heap or not.
+
+Input : [2, 3, 4, 5, 10, 15]
+Output: true
+Explanation: The input represents a min-heap.
+
+		   2
+		 /   \
+		/	  \
+	   3	   4
+	  / \	  /
+	 /   \   /
+	5	 10 15
+
+Input : [2, 10, 4, 5, 3, 15]
+Output: false
+Explanation: The input is not a min-heap, as it violate the heap property.
+
+		   2
+		 /   \
+		/	  \
+	   10	   4
+	  / \	  /
+	 /   \   /
+	5	  3 15
+
+*/
+
+class Solution
+{
+	public static boolean checkMinHeap(int[] nums)
+	{
+		// Write your code here...
+		if (nums == null || nums.length == 0) {
+			return true;
+		}
+		return  check(nums, 0);
+		
+	}
+	
+	private static boolean check(int[] nums, int index) {
+		int n = nums.length;
+		if (index > n / 2) {
+			return true; //leaf node
+		}
+		
+		int leftIndex = 2 * index + 1;
+		int rightIndex = 2 * index + 2;
+		int leftVal = (leftIndex >= n) ? Integer.MAX_VALUE : nums[leftIndex];
+		int rightVal = (rightIndex >= n) ? Integer.MAX_VALUE : nums[rightIndex];
+		if (leftVal < nums[index] || rightVal < nums[index]) {
+			return false;
+		}
+		return check(nums, leftIndex) && check(nums, rightIndex);
+	}
+}
