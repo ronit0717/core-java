3783a6e4bb0331d417ae6c25dbea53493d57052c
ronit0717 <ronit0717@gmail.com>
2025-05-07 12:30:10 +0530

    Insert position binary search

--- lc/SearchInsertPosition.java
+++ lc/SearchInsertPosition.java
@@ -1,3 +1,23 @@
+//Solution 2: TC = O(Lo)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
+
+//Solution 1: TC = O(N)
 class Solution {
     public int searchInsert(int[] nums, int target) {
     	int i;
