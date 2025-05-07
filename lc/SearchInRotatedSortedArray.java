082656bb13c6ba51849473a44980a66ccd8dc2ea
ronit0717 <ronit0717@gmail.com>
2025-05-07 18:08:22 +0530

    Search in rotated sorted array

--- lc/SearchInRotatedSortedArray.java
+++ lc/SearchInRotatedSortedArray.java
@@ -6,29 +6,28 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        int mid;
-        while (start <= end) {
-            mid = (start + end) / 2;
-            
-            if (target == nums[mid]) {
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
                 return mid;
             }
-            
-            if (nums[end] > nums[mid]) { //right half is sorted
-                if (target <= nums[end] && target > nums[mid]) { //target is in right segment
-                    start = mid + 1;
-                } else { //target is in left segment
+            //check sorted part
+            if (nums[start] <= nums[mid]) {
+                //left is sorted
+                if (target < nums[mid] && target >= nums[start]) {
                     end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
-            } else { //left half is sorted
-                if (nums[start] <= target && target < nums[mid]) { //target is in left segment
-                    end = mid - 1;
-                } else { //target is in right segment
+            } else if (nums[mid] <= nums[end]) {
+                //right is sorted
+                if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
         }
-        
         return -1;
     }
 }
