9947ec81708fa3d235dd990fcefe3c0a46f0c8d6
ronit0717 <ronit0717@gmail.com>
2025-05-26 12:11:42 +0530

    Reverse words in a string

--- lc/ReverseWordsInAString.java
+++ lc/ReverseWordsInAString.java
@@ -1,4 +1,48 @@
 /*
+Solution 3: Reverse the string. Then find the word and reverse the word
+*/
+class Solution {
+
+    private static final char space = ' ';
+
+    public String reverseWords(String s) {
+        char[] charArr = s.toCharArray();
+        reverse(0, charArr.length - 1, charArr);
+        StringBuilder sb = new StringBuilder();
+        boolean addSpace = false;
+        for (int i = 0; i < charArr.length; i++) {
+            if (charArr[i] == space) {
+                continue;
+            }
+            int j = i + 1;
+            while(j < charArr.length && charArr[j] != space) {
+                j++;
+            }
+            if (addSpace) {
+                sb.append(space);
+            } else {
+                addSpace = true;
+            }
+            for (int k = j - 1; k >= i; k--) {
+                sb.append(charArr[k]);
+            } 
+            i = j;
+        }   
+        return sb.toString();
+    }
+
+    private void reverse(int start, int end, char[] charArr) {
+        while(start < end) {
+            char temp = charArr[start];
+            charArr[start] = charArr[end];
+            charArr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+/*
 Solution 2: Find the ith index, first non space character
             Then find the next jth index, the first space character after i
             substring(i, j) prepend in string builder
