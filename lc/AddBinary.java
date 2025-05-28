e20c52b04e086dc7fc9023418ca2cb2b8a93016d
ronit0717 <ronit0717@gmail.com>
2025-05-28 12:04:07 +0530

    Code simplified

--- lc/AddBinary.java
+++ lc/AddBinary.java
@@ -1,39 +1,37 @@
 class Solution {
     public String addBinary(String a, String b) {
-        boolean carry = false;
+        if (a.length() < b.length()) {
+            return addBinary(b, a);
+        }
         StringBuilder sb = new StringBuilder();
         int i = a.length() - 1;
         int j = b.length() - 1;
-        while((i != -1) || (j != -1) || carry) {
-        	char ac = '0';
-        	char bc = '0';
-        	if (i != -1) {
-        		ac = a.charAt(i);
-        		i--;
-        	}
-        	if (j != -1) {
-        		bc = b.charAt(j);
-        		j--;
-        	}
-        	if (ac == '1' && bc == '1' && carry) {
-        		sb.insert(0, '1');
-        		carry = true;
-        	} else if (ac == '1' && bc == '1' && !carry) {
-        		sb.insert(0, '0');
-        		carry = true;
-        	} else if ((ac == '1' || bc == '1') && carry) {
-        		sb.insert(0, '0');
-        		carry = true;
-        	} else if ((ac == '1' || bc == '1') && !carry) {
-        		sb.insert(0, '1');
-        		carry = false;
-        	} else if (carry) {
-        		sb.insert(0, '1');
-        		carry = false;
-        	} else {
-        		sb.insert(0, '0');
-        	}
+        int carry = 0;
+        while(i >= 0) {
+            int num1 = a.charAt(i) - '0';
+            int num2 = j < 0 ? 0 : b.charAt(j) - '0';
+            int result = num1 + num2 + carry;
+            if (result == 0) {
+                sb.insert(0, '0');
+                carry = 0;
+            } else if (result == 1) {
+                sb.insert(0, '1');
+                carry = 0;
+            } else if (result == 2) {
+                sb.insert(0, '0');
+                carry = 1;
+            } else if (result == 3) {
+                sb.insert(0, '1');
+                carry = 1;
+            }
+            i--;
+            j--;
+        }
+        if (carry == 1) {
+            sb.insert(0, '1');
         }
         return sb.toString();
     }
+
+
 }
